c7524d195816abac8a217d9669c4f42f326e0ead
Max Rogulin <max@manu.team>
2025-08-13 03:21:22 +0400

    added basic setup and registration module

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import auth from '@/store/modules/auth'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
+  state: {},
   getters: {
   },
   mutations: {
@@ -13,5 +14,6 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
+    auth
   }
 })
