7afb3832a56065ae09db35e7cf72a0f7ddbf2acd
Max Rogulin <max@manu.team>
2025-10-05 19:59:56 +0400

    feat/auth: refactored naming of actions and mutations

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -51,6 +51,7 @@
 
 <script>
 import McvValidationErrors from '@/components/ValidationErrors.vue'
+import {actionTypes} from '@/store/modules/auth'
 
 export default {
   name: 'McvRegister',
@@ -76,7 +77,7 @@ export default {
     onSubmit() {
       console.log('Form submitted!')
       this.$store
-        .dispatch('register', {
+        .dispatch(actionTypes.register, {
           email: this.email,
           username: this.username,
           password: this.password,
